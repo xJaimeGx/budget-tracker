e201099050e1e0abb2b0ee6cdcb1b33951f2779b
xJaimeGx <beanieman019@protonmail.com>
2022-05-08 17:05:26 -0500

    add save and get records, add dependencies

--- public/js/idb.js
+++ public/js/idb.js
@@ -23,3 +23,47 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log(event.target.error);
 };
+
+// save record
+function saveRecord(record) {
+    // create transaction in objectstore
+    const createTransaction = db.transaction('new_transaction', 'readwrite');
+    // create access to the objectstore
+    const accessStore = createTransaction.objectStore('new_transaction');
+    // store record
+    accessStore.add(record);
+};
+
+// get record
+function findRecord() {
+    // create transaction in objectstore
+    const createTransaction = db.transaction('new_transaction', 'readwrite');
+    // create access to the objectstore
+    const accessStore = createTransaction.objectStore('new_transaction');
+    // get every record from objectstore
+    const allRecords = accessStore.allRecords();
+    console.log(allRecords);
+
+    // if getting records is successful, post them
+    allRecords.onsuccess = function() {
+        if (allRecords.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(allRecords.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            // clear indexdb storage
+            .then(response => response.json())
+            .then(() => {
+                const clearTransaction = db.transaction([ 'new_transaction' ], 'readwrite');
+                const clearStore = clearTransaction.objectStore('new_transaction');
+                clearStore.clear();
+            });
+        }
+    };
+};
+
+window.addEventListener('online', findRecord);
