835f1aa5c721153717d60ba58f5fb1c6adb12dfd
xJaimeGx <beanieman019@protonmail.com>
2022-05-08 17:23:36 -0500

    add manifest

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const CACHED_FILES = [
+    '/',
+    '/public/index.html',
+    '/public/css/styles.css',
+    '/public/js/idb.js',
+    '/public/js/index.js',
+    '/public/icons/icon-144x144.png',
+    '/public/icons/icon-384x384.png'
+];
